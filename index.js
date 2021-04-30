810a73cac98abee4654c86e09f9ca37b60b5b1c6
OLEKSANDR <68386153+Alex21777now@users.noreply.github.com>
2021-04-30 17:53:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(app);
 
 const PORT = process.env.PORT || 80
 
